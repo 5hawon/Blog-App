13582d2a6f78bf74ad3026287c11c8de00de9910
5hawon <sshawon993@gmail.com>
2024-04-09 00:35:16 +0600

    Frontend Init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set("views",path.resolve('./views'));
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+app.listen(PORT,()=>{
+    console.log('listening on port '+PORT);
+})
