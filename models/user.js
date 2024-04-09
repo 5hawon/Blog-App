48f108e64e4d019bb6d23c5cf5783d009c818efc
5hawon <sshawon993@gmail.com>
2024-04-09 12:31:07 +0600

    Sign In

--- models/user.js
+++ models/user.js
@@ -48,7 +48,30 @@ userSchema.pre("save", function(next){
     next();
 })
 
+userSchema.static('matchPassword',async function(email,password){
+    const user = await this.findOne({email});
+    if(!user) throw new Error("No such user");
+
+
+    const salt = user.salt;
+
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac('sha256',salt)
+    .update(password)
+    .digest("hex");
+    if(userProvidedHash!==hashedPassword) throw new Error("Authentication failed");
+
+    return user;
+
+
+
+
+
+})
+
 const User = model('user', userSchema);
+
 
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,18 @@ router.get('/signup',(req,res)=>{
     return res.render('signup');
 })
 
+router.post('/signin',async(req,res)=>{
+    const {email, password} = req.body;
+    console.log(email, password);
+    const user = await User.matchPassword(email, password);
+
+    console.log('User: ',user);
+
+    res.redirect('/')
+
+
+})
+
 router.post('/signup', async(req,res)=>{
     console.log(req.body);
     const {fullName, email, password} = req.body;
